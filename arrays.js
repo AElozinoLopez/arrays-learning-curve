e09f7cc3c855b87e0382a0af93380ec058bfb353
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-02-24 07:44:09 +0100

    Added an element to the beginning of the array

--- arrays.js
+++ arrays.js
@@ -1,10 +1,14 @@
 // console.log("Hello World!");
 
 // Adding Elements To An Array
-// This is done by using the push method
+// This is done by using the push method. This method adds the element(s) to the end of the array
 const numbers = [3,4];
 
 numbers.push (5,6)  // Adding multiple elements
 numbers.push (7)
-console.log(numbers);
 
+
+// Use unshift method to add elements to the beginning of the array
+numbers.unshift (1)
+
+console.log(numbers);
