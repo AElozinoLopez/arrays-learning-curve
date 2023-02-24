142524519a04b86cf02600e480e8c8c33427ff9d
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-02-24 13:31:12 +0100

    Using the indexOf() method on primitive type

--- arrays.js
+++ arrays.js
@@ -2,16 +2,16 @@
 
 // Adding Elements To An Array
 // This is done by using the push method. This method adds the element(s) to the end of the array
-const numbers = [3,4];
+// const numbers = [3,4];
 
-numbers.push (5,6)  // Adding multiple elements
-numbers.push (7)
+// numbers.push (5,6)  // Adding multiple elements
+// numbers.push (7)
 
 
 // Use unshift method to add elements to the beginning of the array
-numbers.unshift (1, 2)
+// numbers.unshift (1, 2)
 
-console.log(numbers);
+// console.log(numbers);
 
 // TO add, replace or remove elements at the  middle or any position of an array we use the splice method
 
@@ -29,3 +29,8 @@ console.log(months);
 months.splice(4, 2, 'July' );
 //deletes elemets at index 4 and 5 and adds July at index 4
 console.log(months);
+
+// Finding Elements in An Array
+// The indexOf() method is used. And this also depends on wether you are dealing with primitives or reference types.
+const numbers = [1,2,3,4];
+console.log(numbers.indexOf(‘a’)); 
