2f29df0884175343f4193b32d81bd6e36ae281bb
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-02-24 08:13:45 +0100

    Created an array and used splice to add element

--- arrays.js
+++ arrays.js
@@ -11,5 +11,11 @@ numbers.push (7)
 // Use unshift method to add elements to the beginning of the array
 numbers.unshift (1, 2)
 
-
 console.log(numbers);
+
+// TO add, replace or remove elements at the  middle or any position of an array we use the splice method
+
+const months = ['Jan', 'March', 'April', 'June']
+months.splice (1, 0, 'Feb');  //1 = start position, 0 = delete count (zero means delete nothing), Feb = the element added
+//inserts at index 1
+console.log(months);
