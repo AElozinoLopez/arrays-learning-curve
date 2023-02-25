9d793462ebebeb2df01a25f4ebc40d04c37cdbbd
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-02-25 15:51:16 +0100

    Corrected the concat parameter

--- arrays.js
+++ arrays.js
@@ -164,6 +164,6 @@ console.log(musicalInstruments);
 const firstArray = [1, 2, 3, 4];
 const secondArray = [5, 6, 7, 8];
 
-const combinedArray = firstArray.concat(second);
+const combinedArray = firstArray.concat(secondArray);
 
 console.log(combinedArray);
